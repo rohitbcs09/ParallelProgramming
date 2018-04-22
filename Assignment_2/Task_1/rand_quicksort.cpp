a67368b8fe708c9771f5feb80a9ac6786d635912
mridul <mailtoranjanmridul@gmail.com>
2018-04-22 02:53:23 -0500

    Fixed Quick sort

--- Assignment_2/Task_1/rand_quicksort.cpp
+++ Assignment_2/Task_1/rand_quicksort.cpp
@@ -131,7 +131,7 @@ void insertion_sort(std::vector<int> &arr, int start, int end) {
     int i = start;
     while (i <= end) {
         int j = i;
-        while (j >= 0 && arr[j - 1] > arr[j]) {
+        while (j > start && arr[j - 1] > arr[j]) {
 	    int temp = arr[j - 1];
             arr[j - 1] = arr[j];
             arr[j] = temp;
