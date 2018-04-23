edf33c7808c76a526ba960607b90a60097f2f974
mridul <mailtoranjanmridul@gmail.com>
2018-04-23 00:07:20 -0500

    Output - Radix Sort

---
+++ Assignment_2/Task_2/radix_sort/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./mst_radix 68 ../input_graphs/com-youtube-in.txt output/com-youtube-MST-sort-out.txt
+./mst_radix 68 ../input_graphs/ca-AstroPh-in.txt output/ca-AstroPh-MST-sort-out.txt
+./mst_radix 68 ../input_graphs/com-amazon-in.txt output/com-amazon-MST-sort-out.txt
+./mst_radix 68 ../input_graphs/com-dblp-in.txt output/com-dblp-MST-sort-out.txt
+./mst_radix 68 ../input_graphs/roadNet-PA-in.txt output/roadNet-PA-MST-sort-out.txt
+./mst_radix 68 ../input_graphs/roadNet-TX-in.txt output/roadNet-TX-MST-sort-out.txt
+./mst_radix 68 ../input_graphs/roadNet-CA-in.txt output/roadNet-CA-MST-sort-out.txt
+./mst_radix 68 ../input_graphs/as-skitter-in.txt output/as-skitter-MST-sort-out.txt
+./mst_radix 68 ../input_graphs/com-lj-in.txt output/com-lj-MST-sort-out.txt
