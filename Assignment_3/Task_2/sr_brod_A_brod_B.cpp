f1f7cbfc5d9b947ae801bd621ff318a45067292b
mridul <mailtoranjanmridul@gmail.com>
2018-05-07 17:59:20 -0500

    sr_brod_A_brod_B

---
+++ Assignment_3/Task_2/sr_brod_A_brod_B.cpp
@@ -0,0 +1,402 @@
+#include <iostream>
+#include <mpi.h>
+#include <cmath>
+#include <algorithm>
+#include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
+#include <chrono>
+
+using namespace std;
+
+// Input Matrices
+int **X; 
+int **Y;
+int **Z;
+
+int g_seed;
+
+int processors = 1;
+
+int fastrand() {
+  g_seed = (214013 * g_seed + 2531011); 
+  return (g_seed>>16) & 0x7FFF; 
+}
+
+void create_2d_array(int ***array, int n, int m) {
+
+    int *p = (int *)malloc(n*m*sizeof(int));
+    if(!p) {
+        std::cout << "Malloc Failed !!!\n";
+        return;
+    }
+
+    (*array) = (int**)malloc(n*sizeof(int*));
+    if (!(*array)) {
+       free(p);
+       std::cout << "Malloc Failed !!!\n";
+       return;
+    }
+
+    for (int i=0; i<n; i++) {
+       (*array)[i] = &(p[i*m]);
+    }
+    return;
+}
+
+void delete_2d_array(int ***array) {
+    free(&((*array)[0][0]));
+    free(*array);
+    return;
+}
+
+void fillMatrix(int **arr, int n) {
+    int count = 0;
+    for(int i = 0; i<n; ++i) {
+        for(int j = 0; j<n; ++j) {
+            arr[i][j] = ++count; //fastrand();
+        }
+    }
+}
+
+void fillMatrixVal(int **arr, int n, int val) {
+    for(int i = 0; i<n; ++i) {
+        for(int j = 0; j<n; ++j) {
+            arr[i][j] = val; //fastrand();
+        }
+    }
+}
+
+
+void printMatrix(int **arr, int n) {
+     std::cout << "\n";
+     for(int i = 0; i<n; ++i) {
+        for(int j = 0; j<n; ++j) {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << "\n";
+    }   
+}
+
+void init_sub_matrix(int **arr, int m, int n) {
+    for(int i = 0; i< m; ++i) {
+        for(int j = 0; j< m; ++j) {
+            arr[i][j] = 0;
+        }
+    }
+}
+
+
+void Matrix_Multiply(int **X, int **Y, int **Z, int x_row, int x_col, 
+                     int y_row, int y_col, int z_row, int z_col, int n) {
+    for(int i = 0; i<n; ++i){
+        for(int k = 0; k<n; ++k) {
+            for(int j = 0; j<n; ++j) {
+                Z[z_row + i][z_col + j] += X[x_row + i][x_col + k] * Y[y_row + k][y_col + j]; 
+            }   
+        }   
+    }   
+}
+
+
+
+void PAR_REC_MEM(int **X, int **Y, int **Z, int x_row, int x_col,
+				 int y_row, int y_col, int z_row, int z_col, int n, int base) {
+
+    if(n <= base) {
+        Matrix_Multiply(X, Y, Z, x_row, x_col, y_row, y_col, z_row, z_col, n);
+    }
+    else {
+        cilk_spawn PAR_REC_MEM(X, Y, Z, x_row, x_col, y_row, y_col, z_row, z_col, n/2, base);
+        cilk_spawn PAR_REC_MEM(X, Y, Z, x_row, x_col, y_row, y_col + n/2, z_row, z_col + n/2, n/2, base);
+        cilk_spawn PAR_REC_MEM(X, Y, Z, x_row + n/2, x_col, y_row, y_col, z_row + n/2, z_col, n/2, base);
+        PAR_REC_MEM(X, Y, Z, x_row + n/2, x_col, y_row, y_col + n/2, z_row + n/2, z_col + n/2, n/2, base);
+        cilk_sync;
+        cilk_spawn PAR_REC_MEM(X, Y, Z, x_row, x_col + n/2, y_row + n/2, y_col, z_row, z_col, n/2, base);
+        cilk_spawn PAR_REC_MEM(X, Y, Z, x_row, x_col + n/2, y_row + n/2, y_col + n/2, z_row, z_col + n/2, n/2, base);
+        cilk_spawn PAR_REC_MEM(X, Y, Z, x_row + n/2, x_col + n/2, y_row + n/2, y_col, z_row + n/2, z_col, n/2, base);
+        PAR_REC_MEM(X, Y, Z, x_row + n/2, x_col + n/2, y_row + n/2, y_col + n/2, z_row + n/2, z_col + n/2, n/2, base);
+        cilk_sync;
+    }
+}
+
+
+
+void update_result(int *temp, int rank, int **Z, int n , int p) {
+    int sqrt_p = std::sqrt(p);
+    int proc_mat_size = n / sqrt_p;
+
+    int row = rank / sqrt_p;
+    int col = rank % sqrt_p;
+    int *trav = temp;
+
+    for (int i = row * proc_mat_size; i < row * proc_mat_size + proc_mat_size; ++i) {
+        for (int j = col * proc_mat_size; j < col * proc_mat_size + proc_mat_size; ++j) {
+
+            //std::cout << " i = " << i << " j = " << j << " val = " << *trav;
+            Z[i][j] += *trav;
+            trav++;
+        }
+        //std::cout << "\n";
+    }
+}
+
+int * get_buff_copy(int rank,int  **arr, int size, int procs) {
+    int sqrt_p = std::sqrt(procs);
+    int proc_mat_size = size / sqrt_p;
+
+    int * res = (int *)malloc(proc_mat_size*proc_mat_size*sizeof(int));
+    int *trav = res;
+    int row = rank / sqrt_p;
+    int col = rank % sqrt_p;
+
+    for (int i = row * proc_mat_size; i < row * proc_mat_size + proc_mat_size; ++i) {
+        for (int j = col * proc_mat_size; j < col * proc_mat_size + proc_mat_size; ++j) {
+            *trav = arr[i][j];
+            trav++;
+        }
+    }
+
+    return res;
+}
+
+void MM_broadcast_A_broadcast_B(int n, int p, int rank) {
+    int *mat_X = NULL;
+    int *mat_Y = NULL;
+    int *mat_Z = NULL;
+    int *g_copy_mat = NULL;
+
+    int sqrt_p = std::sqrt(p);
+    int proc_mat_size = n / sqrt_p;
+    int mat_size[2] = {n , n};
+    int sub_mat_size[2] = {proc_mat_size, proc_mat_size};
+    int mat_start[2] = {0, 0};
+
+    int **sub_matrix_X;
+    create_2d_array(&sub_matrix_X, proc_mat_size, proc_mat_size);
+
+    int **sub_matrix_Y;
+    create_2d_array(&sub_matrix_Y, proc_mat_size, proc_mat_size);
+
+    int **sub_matrix_Z;
+    create_2d_array(&sub_matrix_Z, proc_mat_size, proc_mat_size);
+    init_sub_matrix(sub_matrix_Z, proc_mat_size, proc_mat_size);
+
+    MPI_Status status;
+    MPI_Datatype mat_type, sub_mat_type;
+    MPI_Type_create_subarray(2, mat_size, sub_mat_size, mat_start, MPI_ORDER_C, MPI_INT, &mat_type);
+    MPI_Type_create_resized(mat_type, 0, proc_mat_size * sizeof(int), &sub_mat_type);
+    MPI_Type_commit(&sub_mat_type);
+
+    int color = rank % sqrt_p;
+    MPI_Comm col_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, color, rank, &col_comm);
+
+    int row_color = sqrt_p + (rank / sqrt_p);
+    MPI_Comm row_comm;
+    MPI_Comm_split(MPI_COMM_WORLD, row_color, rank, &row_comm);
+
+    int send_mat_count[p]; 
+    int send_mat_start[p];
+
+
+    MPI_Status x_status;
+    MPI_Status y_status;
+    MPI_Status z_status;
+
+    MPI_Request x_request = MPI_REQUEST_NULL;
+    MPI_Request y_request = MPI_REQUEST_NULL;
+    MPI_Request z_request = MPI_REQUEST_NULL;
+
+    int x_tag = 40;
+    int y_tag = 41;
+    int z_tag = 42;
+
+
+
+    if(rank == 0) {
+        int start = 0;
+        for(int i=0; i<sqrt_p; ++i){
+            for(int j=0; j<sqrt_p; ++j){
+                send_mat_count[start]=1;
+                send_mat_start[start++]= (sqrt_p * proc_mat_size * i) + j;
+                //std::cout << (sqrt_p * proc_mat_size * i) + j << " ";
+            }
+        }
+        /*for(int i = 0; i< sqrt_p * sqrt_p; ++i) {
+            std::cout << send_mat_start[i] << " ";
+        }
+        std::cout << "\n";*/
+        mat_X = &(X[0][0]);
+        mat_Y = &(Y[0][0]);
+        mat_Z = &(Z[0][0]);
+
+        // send to all processors
+        for (int i = 0; i < sqrt_p * sqrt_p; ++i) {
+            int *sub_X = get_buff_copy(i, X, n, p);
+            MPI_Isend(sub_X, proc_mat_size * proc_mat_size, MPI_INT, i, x_tag, MPI_COMM_WORLD, &x_request);
+
+            int *sub_Y = get_buff_copy(i, Y, n, p);
+            MPI_Isend(sub_Y, proc_mat_size * proc_mat_size, MPI_INT, i, y_tag, MPI_COMM_WORLD, &y_request);
+
+            int *sub_Z = get_buff_copy(i, Z, n, p);
+            MPI_Isend(sub_Z, proc_mat_size * proc_mat_size, MPI_INT, i, z_tag, MPI_COMM_WORLD, &z_request);
+
+            free(sub_X);
+            free(sub_Y);
+            free(sub_Z);
+        }
+
+    }
+
+    MPI_Irecv(&(sub_matrix_X[0][0]), proc_mat_size * proc_mat_size, MPI_INT, 0, x_tag, MPI_COMM_WORLD, &x_request);
+    MPI_Irecv(&(sub_matrix_Y[0][0]), proc_mat_size * proc_mat_size, MPI_INT, 0, y_tag, MPI_COMM_WORLD, &y_request);
+    MPI_Irecv(&(sub_matrix_Z[0][0]), proc_mat_size * proc_mat_size, MPI_INT, 0, z_tag, MPI_COMM_WORLD, &z_request);
+
+    MPI_Wait(&x_request, &x_status);
+    MPI_Wait(&y_request, &y_status);
+    MPI_Wait(&z_request, &z_status);
+
+
+    /*
+    MPI_Scatterv(mat_X, send_mat_count, send_mat_start, sub_mat_type, &(sub_matrix_X[0][0]),
+                 proc_mat_size * proc_mat_size, MPI_INT, 0, MPI_COMM_WORLD);
+ 
+    MPI_Scatterv(mat_Y, send_mat_count, send_mat_start, sub_mat_type, &(sub_matrix_Y[0][0]),
+                 proc_mat_size * proc_mat_size, MPI_INT, 0, MPI_COMM_WORLD);
+ 
+    MPI_Scatterv(mat_Z, send_mat_count, send_mat_start, sub_mat_type, &(sub_matrix_Z[0][0]),
+                 proc_mat_size * proc_mat_size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    */
+          
+    /*std::cout << "Rank : " << rank << "\n";
+    for(int j = 0; j< proc_mat_size; ++j) {
+        for(int k =0; k< proc_mat_size; ++k) {
+            std::cout << sub_matrix_X[j][k] << " ";
+        }
+        std::cout << "\n";
+    }*/
+
+    int tag = 99;
+    int p_src_row = rank / sqrt_p; 
+    int p_src_col = rank % sqrt_p;
+
+    int x_send_to_p_dst_row = p_src_row;     
+    int x_recv_from_p_src_row = p_src_row;
+
+    int **a_bcast_ptr;
+    create_2d_array(&a_bcast_ptr, proc_mat_size, proc_mat_size);
+    init_sub_matrix(a_bcast_ptr, proc_mat_size, proc_mat_size);
+    int *a_g_bcast_ptr = &(a_bcast_ptr[0][0]);
+
+    int **b_bcast_ptr;
+    create_2d_array(&b_bcast_ptr, proc_mat_size, proc_mat_size);
+    init_sub_matrix(b_bcast_ptr, proc_mat_size, proc_mat_size);
+    int *b_g_bcast_ptr = &(b_bcast_ptr[0][0]);
+    
+    for(int l = 1; l <= sqrt_p; l++) {
+        int k = (l - 1);
+        if (k == p_src_row) {
+           b_bcast_ptr = sub_matrix_Y;
+        }
+
+        if (k == p_src_col) {
+	    a_bcast_ptr = sub_matrix_X;
+        }
+
+        MPI_Bcast(&(b_bcast_ptr[0][0]), proc_mat_size * proc_mat_size, MPI_INT, 
+                  k, col_comm);
+
+        MPI_Bcast(&(a_bcast_ptr[0][0]), proc_mat_size * proc_mat_size, MPI_INT,
+                  k, row_comm);
+
+
+        cilk_spawn PAR_REC_MEM(a_bcast_ptr, b_bcast_ptr, sub_matrix_Z, 0, 0, 0, 0, 0, 0, proc_mat_size, 32);
+        cilk_sync;
+
+        //Matrix_Multiply(a_bcast_ptr, b_bcast_ptr, sub_matrix_Z,
+        //                0, 0, 0, 0, 0, 0, proc_mat_size);
+
+    }
+
+
+    int rcv_tag;
+    MPI_Status rcv_status;
+    MPI_Request rcv_request = MPI_REQUEST_NULL;
+
+    MPI_Isend(&(sub_matrix_Z[0][0]), proc_mat_size * proc_mat_size, MPI_INT, 0, rcv_tag, MPI_COMM_WORLD, &rcv_request);
+
+    if (rank == 0) {
+        for (int i = 0; i < sqrt_p * sqrt_p; ++i) {
+            int *temp = (int *)malloc(proc_mat_size*proc_mat_size*sizeof(int));
+            MPI_Irecv(temp, proc_mat_size * proc_mat_size, MPI_INT, i, rcv_tag, MPI_COMM_WORLD, &rcv_request);
+            MPI_Wait(&rcv_request, &rcv_status);
+            /*
+            std::cout << "rank : " << i  << " TAG - "<< rcv_tag[i] << std::endl;
+            for (int j = 0; j < proc_mat_size * proc_mat_size; ++j) {
+                std::cout << temp[j] << " ";
+            }
+            std::cout << "\n";
+            */
+            update_result(temp, i, Z, n , p);
+            free(temp);
+        }
+    }
+
+    //MPI_Gatherv(&(sub_matrix_Z[0][0]), proc_mat_size * proc_mat_size,  MPI_INT, mat_Z, send_mat_count, 
+    //            send_mat_start, sub_mat_type, 0, MPI_COMM_WORLD);
+    
+    if(rank == 0) {
+        std::cout << "\nMASTER OUTPUT:\n";
+        printMatrix(Z, n);
+    }
+
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    if(argc < 2)  {
+        std::cout << "Missing Input params - ibrun -n <processors> <binary> <matrix_size>\n";
+        return 1;
+    }
+    int n = atoi(argv[1]);
+
+    srand(time(NULL));
+    g_seed=rand();
+
+    int myrank, v = 121;
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Comm_size(MPI_COMM_WORLD, &processors);
+    
+    if(myrank == 0) {
+        create_2d_array(&X, n, n);
+        fillMatrix(X, n);
+        printMatrix(X, n);
+
+        create_2d_array(&Y, n, n);
+        fillMatrix(Y, n);
+
+        create_2d_array(&Z, n, n);
+        init_sub_matrix(Z, n, n);
+    }
+
+
+    using namespace std::chrono;    
+    high_resolution_clock::time_point start_time = high_resolution_clock::now();
+
+    MM_broadcast_A_broadcast_B(n, processors, myrank);
+
+    high_resolution_clock::time_point end_time = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double>>(end_time - start_time);
+
+    if(myrank == 0) {
+        std::cout << "Exectution Time: " << time_span.count() << " seconds.";
+        std::cout << std::endl;
+    }
+    return 1;
+}
+
